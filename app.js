6c9387a79c1354b1fc599161248ebe8808e4bebc
Julio Arias <cesararias2420@gmail.com>
2026-02-23 15:16:53 +0100

    learning-path

--- app.js
+++ app.js
@@ -34,4 +34,51 @@ tareaEspecial.completed = true;
 let tareasFiltradas = misTareas.filter(item => item.id !== 3)
 
 let listaDeTextos = misTareas.map(item => item.text)
-console.log(misTareas);
+
+
+let titulosUrgentes = misTareas.map(item => `Tarea: ${item.text}`)
+
+let soloUrgentes = misTareas.filter(item => item.priority === "high")
+
+tareaEspecial.completed = !tareaEspecial.completed
+
+let tareasActualizadas = misTareas.map(item => 
+    item.id === 1 ? { ...item, priority: "urgent" } : item
+)   
+
+let tareasFiltradas2 = misTareas.filter(item => !item.completed)
+.map(item => ({...item, text: item.text.toUpperCase()}))
+
+
+const actualizarPrioridad = (lista, idACambiar) => {
+  return lista.map(item => 
+    item.id === idACambiar ? {...item, priority: "urgent"} : item
+  );
+};
+
+
+const usuario = "Juan";
+
+const SaludoUsuario = () => {
+  return (
+    <header>
+      <h1>Mi Gestor de Tareas</h1>
+      <p>Bienvenido de nuevo, {usuario}</p>
+    </header>
+  );
+};
+
+const ListaDeTareas = () => {
+  return (
+    <ul>
+      {misTareas.map(item => (
+        <li>{item.text} - Prioridad: {item.priority}</li>
+      ))}
+    </ul>
+  );
+};
+
+
+
+
+AudioParamMap
